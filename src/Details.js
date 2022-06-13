ddb6897b0b3139d1748adbb7925f2971a186e164
micheldavrdz <micheldavrdz@gmail.com>
2022-06-13 02:29:46 -0500

    add: update files to next section
    update files up to start of 'Special Case React Tools' first video

--- src/Details.js
+++ src/Details.js
@@ -1,17 +1,66 @@
+import { Component } from "react";
 import { useParams } from "react-router-dom";
+import Carousel from "./Carousel";
 
-const Details = () => {
-  const { id } = useParams();
+// Cant use hooks in class components (aka useEffect, useParams or anything that uses ´use´)
+class Details extends Component {
+  // constructor(props) {
+  //   super(props);
 
-  return (
-    <div className="details">
-      <h1>Details</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam,
-        voluptatum?
-      </p>
-    </div>
-  );
+  //   this.state = { loading: true };
+  // }
+
+  // This works by using the class properties babel plugin
+  state = { loading: true };
+
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.params.id}`
+    );
+
+    const json = await res.json();
+
+    // this.setState({
+    //   loading: false,
+    // });
+
+    // this.setState(json.pets[0]);
+
+    // Does the same as above but in 1 statement so react gets it all at once
+    this.setState(Object.assign({ loading: false }, json.pets[0]));
+
+    // Could also use a spread operator to do the same thing
+    // this.setState({ loading: false, ...json.pets[0] });
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <h2>Loading...</h2>;
+    }
+
+    // Works the same as above but deconstructed so we don't have to say this.state.animal or this.state.breed every time
+    const { animal, breed, city, state, description, name, images } =
+      this.state;
+
+    return (
+      <div className="details">
+        <Carousel images={images} />
+        <div>
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {city}, {state}
+          </h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+const WrappedDetails = () => {
+  const params = useParams();
+  return <Details params={params} />;
 };
 
-export default Details;
+export default WrappedDetails;
